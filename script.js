808face88acaee9251d64844b1e31fcdfddca477
Clement-k <ANNORCLEMENT64@GMAIL.COM>
2026-06-17 12:22:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function closeMenu() {
     document.getElementById("sideMenu").classList.remove("active");
     document.getElementById("overlay").classList.remove("active");
 }
+
+
+function openMenu() {
+    sideMenu.classList.add("active");
+    overlay.classList.add("active");
+    document.body.classList.add("menu-open");
+}
+
+function closeMenu() {
+    sideMenu.classList.remove("active");
+    overlay.classList.remove("active");
+    document.body.classList.remove("menu-open");
+}
